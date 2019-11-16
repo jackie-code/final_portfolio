e237e065714f07e5c4550ef11eef0146193c7dcf
Jacqeuline Martin <paradiseprofessional88@gmail.com>
2019-11-16 14:00:26 -0500

    flex changes, added images

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 
 
 function showPage() {
-    $('html').show();
+    $('.stuff').show();
 }
 
+function hidePage() {
+    $('.stuff').hide();
+}
+
+function showHeroExpand() {
+    $('.heroExpand').show();
+}
+
+function hideHeroExpand() {
+    $('.heroExpand').hide();
+}
 
 /* **************event listeners****************  */
 function eventListeners(){
 
     $('header').on('click', '.hero', function () {
-        $('html').hide();
-    }
+        hidePage();
+        showHeroExpand();
+        $('header').hide();
+    });
 
+    $('.heroExpand').on('click', '.x', function () {
+        showPage();
+        hideHeroExpand();
+        $('header').show();
+    })
+
+    // here I want to hover over an item and make a larger image associated with that item appear and then dissappeaer on mouseoff
+    //probably will need to set up a store for the item numbers and images with those numbers
+    $('#thegridofdestiny').on('hover', '.item', function(){
+
+    })
 
 }
 
 
 /* ***************get document ready*************** */
-$('document').ready(function startPage() {
+$(document).ready(function startPage() {
     // We call this function ONCE
     eventListeners();
     showPage();
+    hideHeroExpand();
   })
